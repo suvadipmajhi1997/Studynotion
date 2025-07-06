3d6292c245b7a4afd71bdc9c26ca387fc571b367
suvadip majhi <114084926+suvadipmajhi1997@users.noreply.github.com>
2025-07-06 08:22:22 +0530

    navbar added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,45 +1,85 @@
 import React from 'react'
+import logo from '../assets/Logo/Logo-Full-Light.png'
+import { CiSearch } from "react-icons/ci";
+import { PiShoppingCartThin } from "react-icons/pi";
+import { FaRegCircleUser } from "react-icons/fa6";
 
 const Nav = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container d-lg-flex ">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <nav className="navbar navbar-expand-lg px-3">
+        <div className="container d-flex align-items-center justify-content-between">
+          <div className="d-flex align-items-center ms-0 p-0">
+            
+            <button
+              className="navbar-toggler d-lg-none border"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navContent"
+              aria-controls="navbarContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            
+          </div>
+
+          <a
+              className="navbar-brand d-flex d-lg-flex align-items-center"
+              href="#"
+            >
+              <img src={logo} alt="" />
+            </a>
+
+          {/* only for large screen */}
+          <div className="d-none d-lg-flex mx-auto">
+            <ul className="navbar-nav flex-row gap-3">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Catalog
+                </a>
+                <ul className="dropdown-menu">
+    <li><a className="dropdown-item" href="#">Action</a></li>
+    <li><a className="dropdown-item" href="#">Another </a></li>
+    <li><a className="dropdown-item" href="#">Something </a></li>
+  </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact us
+                </a>
+              </li>
+            </ul>
+          </div>
+
+         {/* right icons */}
+         <div className="d-flex align-items-center gap-2" style={{color:"white"}}>
+          <CiSearch size={24}/>
+          <PiShoppingCartThin size={24}/>
+          <FaRegCircleUser size={24}/>
+         </div>
+
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-</div>
-  )
+  );
 }
 
 export default Nav
